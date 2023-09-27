877691f87479ea03124eb1dd022abdc15055f475
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 19:12:19 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const result = {}
+  try {
+    const pic = await uploadPhoto();
+    const usr = await createUser();
+    result = { pic, usr };
+  } catch(error) {
+    result = { pic: null, usr: null };
+  }
+  return result;
+}
