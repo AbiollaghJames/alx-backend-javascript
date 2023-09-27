384d75ef461e8e8aae422f2de8da95d531087b3f
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 20:03:38 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const func1 = signUpUser(firstName, lastName);
   const func2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([ func1, func2 ])
+  return Promise.allSettled([func1, func2])
     .then((data) => {
       data[1].value = `Error: ${data[1].reason.message}`;
       delete data[1].reason;
