2a808e01fdd261d06cddd6188ab15c67ff7a4e2c
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 02:53:49 +0000

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,7 @@
 export default function getFullResponseFromAPI(success) {
-  //resolves if success is true else rejection
-
   return new Promise((resolve, reject) => {
     if (success) {
-      const data = {status: 200, body: 'Success'}
+      const data = { status: 200, body: 'Success' }
       resolve(data);
     } else {
       reject(new Error('The fake API is not working currently'));
