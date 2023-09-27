9fe80c9399d4d7d1acc8cea094948fc769d4b36e
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 05:48:14 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promises = [ uploadPhoto(), createUser() ];
-  return Promise.all(promises)
-    .then((vals) => {
-      console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
-    })
-    .catch((error) => {
-      console.error('Signup system offline');
-    });
+    return Promise.all(promises)
+      .then((vals) => {
+	console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
+      })
+      .catch(() => {
+	console.log('Signup system offline');
+      });
 }
