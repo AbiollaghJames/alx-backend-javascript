f4fe4a9b11fd5f3f90393c40f5c82e5f9668c9ae
AbiollaghJames <abiollajames@protonmail.com>
2023-09-27 19:44:12 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promises = [ uploadPhoto(), createUser() ];
+  let promises = [ uploadPhoto(), createUser() ];
   return Promise.all(promises)
     .then((vals) => {
       console.log(`${vals[0].body} ${vals[1].firstName} ${vals[1].lastName}`);
