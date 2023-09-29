9ee4b2558f0a360b955108a0259040d1a64e1425
AbiollaghJames <abiollajames@protonmail.com>
2023-09-29 02:52:19 +0000

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  set code(val) {
+    if (typeof val !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = val;
+  }
+
+  get [symbol.tString]() {
+    return `${this._code}`;
+  }
+}
