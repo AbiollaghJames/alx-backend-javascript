be7009ee9ed5522a8ada9c4f926c02657e40318d
AbiollaghJames <abiollajames@protonmail.com>
2023-09-29 09:17:33 +0000

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,24 +1,18 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
+    if (this.constructor === Building) {
+      throw new Error('Abstacr class cannot be instantiated');
+    }
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error (
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(val) {
-    if (typeof val !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = val;
-  }
-
-  if (this.constructor !== Building) {
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
-    }
   }
 }
