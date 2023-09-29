bb64c9aabad2ed994bcd5ba093d4c03a0857dc50
AbiollaghJames <abiollajames@protonmail.com>
2023-09-29 03:39:25 +0000

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(val) {
+    this._size = val;
+  }
+
+  set location(val) {
+    this._location = val;
+  }
+
+  [symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+
+    if (hint === 'number') {
+      return this._size;
+    }
+    return this;
+  }
+}
