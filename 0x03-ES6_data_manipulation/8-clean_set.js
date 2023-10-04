9e37503d242e7514b6d0d4da9d8301847e6bff12
AbiollaghJames <118794520+AbiollaghJames@users.noreply.github.com>
2023-10-04 05:13:03 +0300

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || startString === '') {
+  const res = [];
+  if (!(set instanceof Set) || !startString) {
     return '';
   }
 
-  const filteredVal = [...set].filter((value) => value.startsWith(startString));
-  const res = [...filteredVal].map((value) => value.slice(startString.length)).join('-');
-  return res;
+  set.forEach((value) => {
+    if (value && value.startWith(startString)) {
+      res.push(value.slice(startString.length));
+    }
+  });
+  return res.join('-');
 }
