f85aad3dbd9c5338a83475e99bcb23af39ab343e
AbiollaghJames <abiollajames@protonmail.com>
2023-11-21 15:44:11 +0300

    Creating a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,34 @@
-const http = require("http");
-const countStudents = require("./3-read_file_async");
-
-const port = 1245;
-const host = '127.0.0.1';
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 const args = process.argv.slice(2);
 
 const DATABASE = args[0];
 
-const app = http.createServer(async (req, res) => {
-    res.setHeader(200, 'Content-Type', 'text/plain');
+const host = '127.0.0.1';
+const port = 1245;
 
-    const { url } = req;
-    if (url === '/') {
-        res.end("Hello Holberton School!");
-    } else if (url === '/students') {
-        res.write("This is the list of our students");
-        try {
-            const students = await countStudents(DATABASE);
-            res.end(`${students.join('\n')}`);
-        } catch (error) {
-            res.end(error.message);
-        }
+const app = http.createServer(async (req, res) => {
+  res.setHeader(200, 'Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
-    res.statusCode = 404;
-    res.end();
+  }
+  res.statusCode = 404;
+  res.end();
 });
 
 app.listen(port, host, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}/`);
 });
 
 module.exports = app;
