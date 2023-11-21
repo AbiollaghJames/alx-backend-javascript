554c27bbf63bc80e05dd7a0ca0361b16ea92bbc9
AbiollaghJames <abiollajames@protonmail.com>
2023-11-21 11:19:22 +0300

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,3 @@
-/**
- * Log message to the console
- * @param {string} message
- * @returns {void}
- */
-function displayMessage(message) {
+module.exports = function displayMessage (message) {
     console.log(message);
 };
-
-module.exports = displayMessage;
