04db3786f2b2b6e1d143ce1aee29da441a0e4860
AbiollaghJames <abiollajames@protonmail.com>
2023-11-23 14:18:57 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,12 @@
 var assert = require("assert");
-const mocha = require("mocha");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function() {
     it("Should test the rounded part", function() {
-        assert.strictEqual(calculateNumber(1.5, 3.8), 6);
-        assert.strictEqual(calculateNumber(-6.4, -3.8), -10);
-        assert.strictEqual(calculateNumber(0.9, 0.7), 2);
-        assert.strictEqual(calculateNumber(6.9, 8), 15);
-        assert.strictEqual(calculateNumber(5, 8.8), 14);
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
     it("Should sum signed and unsigned integers", function() {
         assert.strictEqual(calculateNumber(3, 4), 7);
